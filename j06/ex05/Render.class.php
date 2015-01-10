cf262c3b3c17e141a2473b7503a88b69d7abd3ce
npineau <npineau@student.42.fr>
2015-01-10 17:34:54 +0100

    Construct and destruct.

--- j06/ex05/Render.class.php
+++ j06/ex05/Render.class.php
@@ -11,5 +11,27 @@ class Render
 	private $_width;
 	private $_height;
 	private $_filename;
+
+	public static $verbose = false;
+
+	public function __construct(array $kwargs)
+	{
+		$this->_width = $kwargs['width'];
+		$this->_height = $kwargs['height'];
+		$this->_filename = $kwargs['filename'];
+
+		if (Triangle::$verbose === true)
+		{
+			print("Render instance constructed\n");
+		}
+	}
+
+	public function __destruct()
+	{
+		if (Triangle::$verbose === true)
+		{
+			print("Render instance destructed\n");
+		}
+	}
 }
 ?>
