cce2161c98d858aae235fb0b2fbdf69a8828bc5a
npineau <npineau@student.42.fr>
2015-01-12 12:58:19 +0100

    ALLLEW

--- j06/ex05/Render.class.php
+++ j06/ex05/Render.class.php
@@ -54,26 +54,52 @@ class Render
 	(int)$miny = (min($Y1, min($Y2, $Y3)) + 0xF) >> 4;
 	(int)$maxy = (max($Y1, max($Y2, $Y3)) + 0xF) >> 4;
 
-	// Block size, standard 8x8 (must be power of two)
-	(int)$q = 8;
-
-	// Start in corner of 8x8 block
-	$minx &= ~($q - 1);
-	$miny &= ~($q - 1);
-
-	//
-
 	//Half-edge constants
 	(int)$C1 = $DY12 * $X1 - $DX12 * $Y1;
 	(int)$C2 = $DY23 * $X2 - $DX23 * $Y2;
 	(int)$C3 = $DY31 * $X3 - $DX31 * $Y3;
+
+	//Correct for fill convention.
+	if ($DY12 < 0 || ($DY12 == 0 && $DX12 > 0))
+	{$C1++;}
+	if ($DY23 < 0 || ($DY23 == 0 && $DX23 > 0))
+	{$C2++;}
+	if ($DY31 < 0 || ($DY31 == 0 && $DX31 > 0))
+	{$C3++;}
+
+				(int)$CY1 = $C1 + $DX12 * ($miny << 4) - $DY12 * ($minx << 4);
+	(int)$CY2 = $C2 + $DX23 * ($miny << 4) - $DY23 * ($minx << 4);
+	(int)$CY3 = $C3 + $DX31 * ($miny << 4) - $DY31 * ($minx << 4);
+
+	for((int)$y = $miny; $y < $maxy; $y++)
+	{
+		(int)$CX1 = $CY1;
+		(int)$CX2 = $CY2;
+		(int)$CX3 = $CY3;
+
+		for((int)$x = $minx; $x < $maxx; $x++)
+		{
+			if($CX1 > 0 && $CX2 > 0 && $CX3 > 0)
+			{
+				imagesetpixel($this->_image, $x, $y, $triangle->getA()->getColor()->toPngColor($this->_image));
+			}
+
+			$CX1 -= $FDY12;
+			$CX2 -= $FDY23;
+			$CX3 -= $FDY31;
+		}
+
+		$CY1 += $FDX12;
+		$CY2 += $FDX23;
+		$CY3 += $FDX31;
+	}
 }
 
 
 	public function bibresenham(Vertex $origin, Vertex $left, Vertex $right)
 	{
 
-		$test = 0;
+		(int)$test = 0;
 		$ox[0] = round($origin->getX());
 		$oy[0] = round($origin->getY());
 		$ex[0] = round($left->getX());
@@ -194,7 +220,7 @@ class Render
 			}
 			break;
 		case Render::RASTERIZE:
-			$this->bibresenham($triangle->getA(), $triangle->getB(), $triangle->getC());
+			$this->rasterize($triangle);
 			break;
 		}
 	}
