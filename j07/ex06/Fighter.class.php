ba741e9c24424306265e75be09490340a8aa33e8
npineau <npineau@student.42.fr>
2015-01-13 15:20:44 +0100

    ex06 gg

---
+++ j07/ex06/Fighter.class.php
@@ -0,0 +1,18 @@
+<?php
+abstract class Fighter
+{
+	protected $_type;
+
+	public function __construct($type)
+	{
+		$this->_type = $type;
+	}
+
+	public function get_type()
+	{
+		return ($this->_type);
+	}
+
+	abstract public function fight($target);
+}
+?>
